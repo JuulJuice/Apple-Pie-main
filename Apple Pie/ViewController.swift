3eacba0f7e3825aff2c901951b95337987a1ab2d
Julian <taz38877@msn.com>
2021-11-11 08:36:18 -0500

    Initial Commit

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Lisette Antigua on 11/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var listOfWords: [String] = ["java", "wood", "book", "dog"]
+    
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0 {
+        didSet {
+            newRound()
+     }
+}
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+    }
+}
+
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    newRound()
+    }
+
+    
+    var currentGame: Game!
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+    }
+
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+
+}
+
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining:
+                                incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        }
+    else {
+        enableLetterButtons(false)
+    }
+}
+
+
+
+
+
+    
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    @IBOutlet var letterButtons: [UIButton]!
+
+
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+    }
+        else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+    }
+        else {
+            updateUI()
+    }
+}
+
+    
+    
+}
+
---
+++ Apple Pie/game.swift
@@ -0,0 +1,48 @@
+//
+//  game.swift
+//  Apple Pie
+//
+//  Created by Julian Jackson on 11/10/21.
+//
+
+import Foundation
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+
+
+
+
+
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            }
+    else {
+        guessedWord += "_"
+     }
+    }
+        return guessedWord
+     }
+    
+    
+
+    
+    
+    
+}
+
+
+
+
+
